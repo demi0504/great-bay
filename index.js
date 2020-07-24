dbd6400de6be34b2bb158e858fd4f7bc6574cff7
demi0504 <ripley.demi@gmail.com>
2020-07-23 21:14:55 -0500

    added functions

--- index.js
+++ index.js
@@ -25,16 +25,46 @@ connection.connect(function(err) {
 ​
 // function which prompts the user for what action they should take
 function start() {
-​
+​   inquirer
+        .prompt([
+            {
+            type: "list",
+            name: "choices",
+            message: "Would you like to bid on an item or post an item?",
+            choices: ["BID", "POST"]
+            },
+        ])
+        .then(function (response) {
+            if (response.choices === "POST") {
+                postAuction();
+            } else (response.choices === "BID") {
+                bidAuction();
+            }
+        }) 
 }
 ​
 // function to handle posting new items up for auction
 function postAuction() {
   // prompt for info about the item being put up for auction
-​
+​   inquirer
+        .prompt([
+            {
+              type: "input",
+              name: "item",
+              message: "What are you selling?"
+            },
+            {
+              type: "input",
+              name: "price",
+              message: "What is your starting bid?"
+            },
+        ])
+        .then(function (response) {
+          //create query for database
+        })
 }
 ​
 function bidAuction() {
   // query the database for all items being auctioned
-​
+​   
 }
